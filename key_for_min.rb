72b15e44aaaae7f6a73f452e2fe07856d7c192a1
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-16 08:23:49 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  hash.each do |key, value|
+  hash.sort do |key, value|
     key <=> value
   end.first[0]
 end
